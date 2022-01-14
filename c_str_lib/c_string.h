7496438d97e9f0f22ebc8a0e595bb020859b2912
Mykola Morgunenko <Olli25010@gmail.com>
2022-01-14 23:03:05 +0100

    refactored size_m -> m_size, capacity_m -> m_capacity, data -> m_data

--- c_str_lib/c_string.h
+++ c_str_lib/c_string.h
@@ -7,12 +7,14 @@
 
 #include <stdlib.h>
 #include <stdint.h> // For size_t
+
 #error "APPS_UCU_ERROR: some of the important includes are missing"
+
 #include <stdio.h>
 
 /* ############## ERROR codes ############### */
 // Show that character or substring cannot be found
-#define NOT_FOUND_CODE (-1) 
+#define NOT_FOUND_CODE (-1)
 // Failed to allocate memory. All functions that directly or
 // indirectly (a.e. call my_str_reserve) allocate memory may return this code
 #define MEMORY_ALLOCATION_ERR (-2)
@@ -38,11 +40,10 @@
 
 // Students can define additional error codes.
 
-//! TODO: rename data --> data_m.
 typedef struct {
-    size_t capacity_m; // Allocated memory block size
-    size_t size_m;     // Actual size of the string
-    char *data;        // Pointer to the data block - address in memory where the allocated block begins
+    size_t m_capacity; // Allocated memory block size
+    size_t m_size;     // Actual size of the string
+    char *m_data;        // Pointer to the data block - address in memory where the allocated block begins
 } my_str_t;
 
 // ###########################################################
@@ -72,7 +73,7 @@ typedef struct {
  *      NULL_PTR_ERR if str points to NULL
  *      MEMORY_ALLOCATION_ERR if there was an error while allocating memory for the buffer
  */
-int my_str_create(my_str_t* str, size_t buf_size);
+int my_str_create(my_str_t *str, size_t buf_size);
 
 /*
  * Frees buffer for the given my_str_t structure and sets NULL to the corresponding pointer.
@@ -86,7 +87,7 @@ int my_str_create(my_str_t* str, size_t buf_size);
  * Returns:
  *     0 always
  */
-int my_str_free(my_str_t* str);
+int my_str_free(my_str_t *str);
 
 /*
  * Makes content of the my_str_t-string the same as c-string.
@@ -108,7 +109,7 @@ int my_str_free(my_str_t* str);
  *      BUFF_SIZE_ERR if buf_size is incorrect (too small)
  *      MEMORY_ALLOCATION_ERR if there occurred an error while allocating memory.
  */
-int my_str_from_cstr(my_str_t* str, const char* cstr, size_t buf_size);
+int my_str_from_cstr(my_str_t *str, const char *cstr, size_t buf_size);
 
 // ###########################################################
 // ########################### string information ############
@@ -119,20 +120,20 @@ int my_str_from_cstr(my_str_t* str, const char* cstr, size_t buf_size);
  * if str == NULL should return 0.
  * 
  */
-size_t my_str_size(const my_str_t* str);
+size_t my_str_size(const my_str_t *str);
 
 /*
  * Returns the size of the allocated memory for my_str_t
- * Note: if you implemented the tip above, returns (capacity_m-1) because one byte is reserved for the \0 symbol.
+ * Note: if you implemented the tip above, returns (m_capacity-1) because one byte is reserved for the \0 symbol.
  * If str == NULL should return 0.
  */
-size_t my_str_capacity(const my_str_t* str);
+size_t my_str_capacity(const my_str_t *str);
 
 /*
  * Returns 1 if the string is empty.
  * If str == NULL should return 1 (true).
  */
-int my_str_empty(const my_str_t* str);
+int my_str_empty(const my_str_t *str);
 
 // ###########################################################
 // #################### character access #####################
@@ -149,7 +150,7 @@ int my_str_empty(const my_str_t* str);
  * Note: does not return char because of the need to also return the error codes and
  * general API uniformity.
  */
-int my_str_getc(const my_str_t* str, size_t index);
+int my_str_getc(const my_str_t *str, size_t index);
 
 /*
  * Places a given symbol at the given position in my_str_t string, pointed by str
@@ -158,22 +159,22 @@ int my_str_getc(const my_str_t* str, size_t index);
  * Returns:
  *      0 if OK
  *      NULL_PTR_ERR if the str is NULL
- *      RANGE_ERR if the index is bad -- out of the range [0, str->size_m).
+ *      RANGE_ERR if the index is bad -- out of the range [0, str->m_size).
  */
-int my_str_putc(my_str_t* str, size_t index, char c);
+int my_str_putc(my_str_t *str, size_t index, char c);
 
 /*
  * Returns a c-string with the same content as my_str_t string, pointed by str.
  * The resulting c-string is constant and becomes invalid if my_str_t content is modified.
  * Time complexity should be O(1)!
  *
- * If the buffer data_m has one reserved byte (maximal size_m == capacity_m - 1), implementation
- * can just put the \0 char at the end of the actual string and return the data_m pointer.
+ * If the buffer m_data has one reserved byte (maximal m_size == m_capacity - 1), implementation
+ * can just put the \0 char at the end of the actual string and return the m_data pointer.
  * 
  * Returns:
  *      NULL if str is NULL, otherwise the appropriate c-string.
  */
-const char* my_str_get_cstr(my_str_t* str);
+const char *my_str_get_cstr(my_str_t *str);
 
 // #########################################################################################
 // ################# string modifications ##################################################
@@ -189,7 +190,7 @@ const char* my_str_get_cstr(my_str_t* str);
  * If reserve == false -> use "to" string's capacity if it is enough, otherwise reserve minimal
  * required capacity to hold the "from" string's content.
  *
- * Note 1: if !reserve (reserve == false) -- should reallocate only if to->data_m memory block
+ * Note 1: if !reserve (reserve == false) -- should reallocate only if to->m_data memory block
  * is too small for the from string contents.
  * Note 2: do not forget to free "to" string's old buffer if you allocate
  * a new one -- my_str_reserve() should take care of this.
@@ -199,7 +200,7 @@ const char* my_str_get_cstr(my_str_t* str);
  *      NULL_PTR_ERR if at least one of the strings is NULL
  *      MEMORY_ALLOCATION_ERR if there was an error during memory allocation
  */
-int my_str_copy(const my_str_t* from,  my_str_t* to, int reserve);
+int my_str_copy(const my_str_t *from, my_str_t *to, int reserve);
 
 /*
  * Clears the content of the string, does nothing if the str is NULL.
@@ -209,52 +210,52 @@ int my_str_copy(const my_str_t* from,  my_str_t* to, int reserve);
  *      always 0
  * Alternatively may return NULL_PTR_ERR if str is NULL (using the fail-fast principle)
  */
-int my_str_clear(my_str_t* str);
+int my_str_clear(my_str_t *str);
 
 /*
  * Inserts the given char at the given position in str, shifts all the following symbols
  * to the right.
  * Increases the capacity if needed.
  *
- * Note: Insert at the end -- position data_m[size_m] is allowed.
+ * Note: Insert at the end -- position m_data[m_size] is allowed.
  *
  * Returns:
  *      0 if OK,
  *      NULL_PTR_ERR if str is NULL,
- *      RANGE_ERR if position is bad -- out of the range [0, size_m],
+ *      RANGE_ERR if position is bad -- out of the range [0, m_size],
  *      MEMORY_ALLOCATION_ERR if occurred error during memory allocation.
  */
-int my_str_insert_c(my_str_t* str, char c, size_t pos);
+int my_str_insert_c(my_str_t *str, char c, size_t pos);
 
 /*
  * Inserts the "str" string into the "from" string at the given position,
  * shifts all the following symbols to the right.
  * Increases the capacity if needed.
  *
- * Note: Insert at the end -- position data_m[size_m] is allowed.
+ * Note: Insert at the end -- position m_data[m_size] is allowed.
  *
  * Returns:
  *      0 if OK,
  *      NULL_PTR_ERR if str or from is NULL,
- *      RANGE_ERR if position is bad -- out of the range [0, size_m],
+ *      RANGE_ERR if position is bad -- out of the range [0, m_size],
  *      MEMORY_ALLOCATION_ERR if there was an error during memory allocation.
  */
-int my_str_insert(my_str_t* str, const my_str_t* from, size_t pos);
+int my_str_insert(my_str_t *str, const my_str_t *from, size_t pos);
 
 /*
  * Inserts given c-string from into given my_str_t string str,
  * shifts all the following symbols to the right.
  * Increases the capacity if needed.
  *
- * Note: Insert at the end -- position data_m[size_m] is allowed.
+ * Note: Insert at the end -- position m_data[m_size] is allowed.
  *
  * return:
  *      0 if OK,
  *      NULL_PTR_ERR if str or from is NULL,
- *      RANGE_ERR if position is bad -- out of the range [0, size_m],
+ *      RANGE_ERR if position is bad -- out of the range [0, m_size],
  *      MEMORY_ALLOCATION_ERR if there was an error during memory allocation.
  */
-int my_str_insert_cstr(my_str_t* str, const char* from, size_t pos);
+int my_str_insert_cstr(my_str_t *str, const char *from, size_t pos);
 
 /*
  * Appends the "from" my_str-string to "str" string.
@@ -265,7 +266,7 @@ int my_str_insert_cstr(my_str_t* str, const char* from, size_t pos);
  *      NULL_PTR_ERR if from or str is NULL,
  *      MEMORY_ALLOCATION_ERR if there was an error during memory allocation.
  */
-int my_str_append(my_str_t* str, const my_str_t* from);
+int my_str_append(my_str_t *str, const my_str_t *from);
 
 /*
  * appends c-string from to my_str_t string str.
@@ -276,7 +277,7 @@ int my_str_append(my_str_t* str, const my_str_t* from);
  *      NULL_PTR_ERR if from or str is NULL,
  *      MEMORY_ALLOCATION_ERR if there was an error during memory allocation.
  */
-int my_str_append_cstr(my_str_t* str, const char* from);
+int my_str_append_cstr(my_str_t *str, const char *from);
 
 /*
  * Appends a character to my_str_t string str.
@@ -287,7 +288,7 @@ int my_str_append_cstr(my_str_t* str, const char* from);
  *      NULL_PTR_ERR if str is NULL,
  *      MEMORY_ALLOCATION_ERR if there was an error during allocating memory for the buffer.
  */
-int my_str_append_c(my_str_t* str, char c);
+int my_str_append_c(my_str_t *str, char c);
 
 /*
  * Copies a substring of "from" string to "to" string in bounds [beg, end) --
@@ -301,7 +302,7 @@ int my_str_append_c(my_str_t* str, char c);
  *      RANGE_ERR if boundaries are bad,
  *      MEMORY_ALLOCATION_ERR if there was an error during memory allocation.
  */
-int my_str_substr(const my_str_t* from, my_str_t* to, size_t beg, size_t end);
+int my_str_substr(const my_str_t *from, my_str_t *to, size_t beg, size_t end);
 
 /*
  * Copies a substring of "from" string to "to" c-string in bounds [beg, end) --
@@ -315,7 +316,7 @@ int my_str_substr(const my_str_t* from, my_str_t* to, size_t beg, size_t end);
  *      NULL_PTR_ERR if from or to is NULL,
  *      RANGE_ERR if boundaries are bad.
  */
-int my_str_substr_cstr(const my_str_t* from, char* to, size_t beg, size_t end);
+int my_str_substr_cstr(const my_str_t *from, char *to, size_t beg, size_t end);
 
 /*
  * Deletes a substring in bounds [beg, end), shifts the remainder to the left,
@@ -328,7 +329,7 @@ int my_str_substr_cstr(const my_str_t* from, char* to, size_t beg, size_t end);
  *      RANGE_ERR if boundaries are bad,
  *      NULL_PTR_ERR if str is NULL.
  */
-int my_str_erase(my_str_t* str, size_t beg, size_t end);
+int my_str_erase(my_str_t *str, size_t beg, size_t end);
 
 /*
  * Deletes the last symbol from the my_str_t string srt and returns it.
@@ -338,7 +339,7 @@ int my_str_erase(my_str_t* str, size_t beg, size_t end);
  *      RANGE_ERR if the size of the string is already empty,
  *      NULL_PTR_ERR if the str is NULL.
  */
-int my_str_popback(my_str_t* str);
+int my_str_popback(my_str_t *str);
 
 // ###########################################################
 // ########### String buffer and size modifications ##########
@@ -356,7 +357,7 @@ int my_str_popback(my_str_t* str);
  *      NULL_PTR_ERR if str is NULL,
  *      MEMORY_ALLOCATION_ERR if there was an error during memory allocation.
  */
-int my_str_reserve(my_str_t* str, size_t buf_size);
+int my_str_reserve(my_str_t *str, size_t buf_size);
 
 /*
  * Shrinks the buffer of the my_str_t string str to its actual size.
@@ -370,7 +371,7 @@ int my_str_reserve(my_str_t* str, size_t buf_size);
  *      NULL_PTR_ERR if str is NULL,
  *      MEMORY_ALLOCATION_ERR if there was an error during memory allocation.
  */
-int my_str_shrink_to_fit(my_str_t* str);
+int my_str_shrink_to_fit(my_str_t *str);
 
 /*
  * Resizes my_str_t string str to the new_size.
@@ -385,7 +386,7 @@ int my_str_shrink_to_fit(my_str_t* str);
  *      NULL_PTR_ERR if str is NULL,
  *      MEMORY_ALLOCATION_ERR if there was an error during memory allocation.
  */
-int my_str_resize(my_str_t* str, size_t new_size, char sym);
+int my_str_resize(my_str_t *str, size_t new_size, char sym);
 
 // ################################################################################################
 // ############ lookup and comparison functions ###################################################
@@ -414,7 +415,7 @@ int my_str_resize(my_str_t* str, size_t new_size, char sym);
  *      0 if OK,
  *      NOT_FOUND_CODE if not found.
  */
-size_t my_str_find(const my_str_t* str, const my_str_t* tofind, size_t from);
+size_t my_str_find(const my_str_t *str, const my_str_t *tofind, size_t from);
 
 /*
  * Compares two my_str_t strings like the ordinary c-strings -- in lexicographical order.
@@ -426,7 +427,7 @@ size_t my_str_find(const my_str_t* str, const my_str_t* tofind, size_t from);
  *      -1 (or another value < 0) if str1 < str2,
  *       1 (or another value > 0) if str1 > str2.
  */
-int my_str_cmp(const my_str_t* str1, const my_str_t* str2);
+int my_str_cmp(const my_str_t *str1, const my_str_t *str2);
 
 /*
  * Compares a my_str_t string and a c-string in lexicographical order.
@@ -438,7 +439,7 @@ int my_str_cmp(const my_str_t* str1, const my_str_t* str2);
  *      -1 (or another value < 0) if str1 < cstr2,
  *       1 (or another value > 0) if str1 > cstr2.
  */
-int my_str_cmp_cstr(const my_str_t* str1, const char* cstr2);
+int my_str_cmp_cstr(const my_str_t *str1, const char *cstr2);
 
 /*
  * Finds the given symbol in my_str_t string "str", starting from "from", inclusive.
@@ -449,7 +450,7 @@ int my_str_cmp_cstr(const my_str_t* str1, const char* cstr2);
  *      index of the symbol if it exists in the string,
  *      NOT_FOUND_CODE if it's not in the string.
  */
-size_t my_str_find_c(const my_str_t* str, char tofind, size_t from);
+size_t my_str_find_c(const my_str_t *str, char tofind, size_t from);
 
 // TODO: rename to my_str_find_c_if for the uniformity.
 /*
@@ -462,7 +463,7 @@ size_t my_str_find_c(const my_str_t* str, char tofind, size_t from);
  *      index of the symbol that satisfies the predicate,
  *      NOT_FOUND_CODE if it's not in the string.
  */
-size_t my_str_find_if(const my_str_t* str, size_t beg, int (*predicate)(int));
+size_t my_str_find_if(const my_str_t *str, size_t beg, int (*predicate)(int));
 
 // ########################################################################################
 // ############################ input and output functions ################################
@@ -487,7 +488,7 @@ size_t my_str_find_if(const my_str_t* str, size_t beg, int (*predicate)(int));
  *      MEMORY_ALLOCATION_ERR if there was an error during memory allocation,
  *      IO_READ_ERR if an error occurred while reading.
  */
-int my_str_read_file(my_str_t* str, FILE* file);
+int my_str_read_file(my_str_t *str, FILE *file);
 
 /*
  * Reads the content of stdin and saves it to the my_str-string.
@@ -501,7 +502,7 @@ int my_str_read_file(my_str_t* str, FILE* file);
  *      MEMORY_ALLOCATION_ERR if there was an error during memory allocation,
  *      IO_READ_ERR if an error occurred while reading.
  */
-int my_str_read(my_str_t* str);
+int my_str_read(my_str_t *str);
 
 /*
  * Reads the file's content and saves it to the my_str_t string "str",
@@ -517,7 +518,7 @@ int my_str_read(my_str_t* str);
  *      MEMORY_ALLOCATION_ERR if there was an error during memory allocation,
  *      IO_READ_ERR if an error occurred while reading.
  */
-int my_str_read_file_delim(my_str_t* str, FILE* file, char delimiter);
+int my_str_read_file_delim(my_str_t *str, FILE *file, char delimiter);
 
 /*
  * Writes the content of the given my_str_t string "str" to the given file.
@@ -527,7 +528,7 @@ int my_str_read_file_delim(my_str_t* str, FILE* file, char delimiter);
  *      NULL_PTR_ERR if str or file is NULL,
  *      IO_WRITE_ERR if an error occurred while writing.
  */
-int my_str_write_file(const my_str_t* str, FILE* file);
+int my_str_write_file(const my_str_t *str, FILE *file);
 
 /*
  * Writes content of the given my_str_t string "str" to the stdout.
@@ -537,6 +538,6 @@ int my_str_write_file(const my_str_t* str, FILE* file);
  *      NULL_PTR_ERR if str is NULL,
  *      IO_WRITE_ERR if an error occurred while writing.
  */
-int my_str_write(const my_str_t* str);
+int my_str_write(const my_str_t *str);
 
 #error "APPS_UCU_ERROR: please check functions' names and signatures before deleting this error!
